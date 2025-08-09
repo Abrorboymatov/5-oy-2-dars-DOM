d25570b9c312fcefad5847d778a8c6a432982837
Abror git config --global user.name <abrorboymatov2005@gmail.com>
2025-08-09 22:54:31 +0500

    vazifalar

--- main.js
+++ main.js
@@ -1,4 +1,37 @@
-let listlar = document.querySelectorAll("#listlar li");
-let natija =document.getElementById("natija");
+// 1-misol
+// let listlar = document.querySelectorAll("#listlar li");
+// let natija =document.getElementById("natija");
 
-let 
+// let Text = "";
+
+//   listlar.forEach(item => {
+//     Text += item.textContent + " ";
+//   });
+
+//   natija.append(Text);
+
+
+//   2-misol
+//   const listlarr = document.querySelectorAll("#listlar li");
+
+//   listlarr.forEach(item => {
+//     const number = parseInt(item.textContent); 
+
+//     if (number % 2 === 0) {
+//       item.classList.add("even"); 
+//     } else {
+//       item.classList.add("odd"); 
+//     }
+//   });
+
+
+// 3-misol
+//   let names = ["Sardor", "Fozil", "Qodir", "Sanjar", "Abdulla"];
+//   let natija = document.getElementById("Ismlar");
+//   names.forEach(name => {
+//     let p = document.createElement("p");
+//     p.textContent = name;
+//     natija.prepend(p); 
+//   });
+
+
